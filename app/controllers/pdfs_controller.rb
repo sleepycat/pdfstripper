9e90f5525b0f8d789158ad0325ea48be1dd03894
Mike Williamson <blessedbyvirtuosity@gmail.com>
2011-04-06 13:15:21 -0400

    Proper handling of empty uploads.

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -6,25 +6,30 @@ class PdfsController < ApplicationController
 
   # POST /pdfs
   def create
-    #look at content_type and original filename to determine if this is a PDF:
-    if params[:pdfs][:pdf].content_type == 'application/pdf' and params[:pdfs][:pdf].original_filename.split('.')[-1] == 'pdf'
-    
-    output_filename = "clean_#{strip_chars(params[:pdfs][:pdf].original_filename)}"
-
-    %x{ gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=tmp/#{output_filename} -c .setpdfwrite -f #{params[:pdfs][:pdf].tempfile.path} }
-
-    output_file = File.read("tmp/#{output_filename}")
-    
-    respond_to do |format|
+    if params[:pdfs]
+      #look at content_type and original filename to determine if this is a PDF:
+      if params[:pdfs][:pdf].content_type == 'application/pdf' and params[:pdfs][:pdf].original_filename.split('.')[-1] == 'pdf'
       
-        format.html do
-          send_data output_file.to_s, :disposition => 'inline', :filename => output_filename, :type => 'application/pdf', :x_sendfile => true
+      output_filename = "clean_#{strip_chars(params[:pdfs][:pdf].original_filename)}"
+
+      %x{ gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=tmp/#{output_filename} -c .setpdfwrite -f #{params[:pdfs][:pdf].tempfile.path} }
+
+      output_file = File.read("tmp/#{output_filename}")
+      
+      respond_to do |format|
+          format.html do
+            send_data output_file.to_s, :disposition => 'inline', :filename => output_filename, :type => 'application/pdf', :x_sendfile => true
+        end
       end
+      else
+       #this is not a pdf file. Let them know.
+        flash.notice = t('not_a_pdf')
+        redirect_to :action => 'index'
     end
-    else
-     #this is not a pdf file. Let them know.
-      flash.notice = t('not_a_pdf')
-      redirect_to :action => 'index'
+  else
+    #the form was sent without a file
+    flash.notice = t('no_file')
+    render :action => 'index'
   end
  end
   
@@ -32,6 +37,7 @@ class PdfsController < ApplicationController
 
   def strip_chars filename
     #remove problematic charaters from the filename
-    filename.gsub(/\'|\"|\(|\)/,'').gsub(' ', '_').to_s
+    filename.chomp(".pdf").gsub(' ', '_').gsub(/\W/,'') + ".pdf"
   end
+
 end
